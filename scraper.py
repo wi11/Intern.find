baed749c06066781cfb60dc805fee321ef6ee4a9
William Yu <will187651@gmail.com>
2017-12-20 03:31:03 -0800

    Functions now loop through all job listings on page

--- scraper.py
+++ scraper.py
@@ -1,32 +1,20 @@
-import requests
 import bs4
 from bs4 import BeautifulSoup
 
-import pandas as pd
-import time
-
-URL = "https://www.indeed.com/jobs?q=Software+intern&start=10"
-
-#request the URL above
-page = requests.get(URL)
-
-#use html parser so that python can read the compnents of the page
-soup = BeautifulSoup(page.text, "html.parser")
-
-def extract_job_title(soup):
+def extract_job_titles(soup, listings):
     """ Looks through every div tag with attribute class='row' and then every anchor
     tag within those divs with attribute data-tn-element='jobTitle' because these
     are the html elements with the job title for each job. We want to take these
     titles for each job so we can see what jobs we're scraping"""
     jobs = []
-    for div_tag in soup.find_all(name="div", attrs={"class":"row"}):
+    for div_tag in listings:
         for a_tag in div_tag.find_all(name="a", attrs={"data-tn-element" : "jobTitle"}):
             jobs.append(a_tag["title"])
     return jobs
 
-def extract_job_companies(soup):
+def extract_job_companies(soup, listings):
     companies = []
-    for div_tag in soup.find_all(name="div", attrs={"class":"row"}):
+    for div_tag in listings:
         spans_with_company_class = div_tag.find_all(name="span", attrs={"class":"company"})
         if len(spans_with_company_class) > 0:
             for company_name_span in spans_with_company_class:
@@ -38,9 +26,9 @@ def extract_job_companies(soup):
                 companies.append(company_name_span.text.strip())
     return companies
 
-def extract_job_locations(soup):
+def extract_job_locations(soup, listings):
     locations = []
-    location_spans = soup.findAll(name="span", attrs={"class":"location"})
-    for location_span in location_spans:
-        locations.append(location_span.text.strip())
+    for div_tag in listings:
+        for location_span in div_tag.find_all(name="span", attrs={"class":"location"}):
+            locations.append(location_span.text.strip())
     return locations
